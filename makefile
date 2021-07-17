4296540dc4539a50723925f59d1bb85cfb3909a5
EC2 Default User <ec2-user@ip-172-31-32-188.us-east-2.compute.internal>
2021-07-17 07:14:03 +0000

    adding to git hun

---
+++ makefile
@@ -0,0 +1,10 @@
+pruthvi.exe:main.o fact.o big3.o rev.o
+	gcc -o pruthvi.exe main.o fact.o big3.o rev.o
+main.o:main.c
+	gcc -c main.c
+fact.o:fact.c
+	gcc -c fact.c
+big3.o:big3.c
+	gcc -c big3.c
+rev.0:rev.c
+	gcc -c rev.c
---
+++ rev.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void reverse() {
+    int n, rev = 0, remainder;
+    printf("Enter an integers to be reverse : ");
+    scanf("%d", &n);
+    while (n != 0) {
+        remainder = n % 10;
+        rev = rev * 10 + remainder;
+        n /= 10;
+    }
+    printf("Reversed number = %d\n", rev);
+   // return 0;
+}
+
